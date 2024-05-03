c2d138bdd01d1173ef1ac50f83228805cd610c8e
fadlanrr26 <ramadanfadlan398@gmail>
2024-05-03 13:33:51 +0700

    test2

--- example-app/resources/views/landingpage.blade.php
+++ example-app/resources/views/landingpage.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>fadlan</title>
+    <title>EduTrack</title>
     <style>
         body {
             font-family: sans-serif;
